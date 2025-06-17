36d4ffa4508c0021bdc8e7d72d884a0ee3401ed8
shashankkmciv18 <shashankkm.civ18@itbhu.ac.in>
2025-06-17 23:49:27 +0530

    Mock

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ from db import init_db
 app = FastAPI()
 init_db()
 app = FastAPI()
-app.include_router(mock_router, prefix="", tags=["YouTube Upload"])
+app.include_router(mock_router, prefix="", tags=["Mock"])
